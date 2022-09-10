af31bef15523a31da5110cc8bc55398d66ccdf45
Rob Crowther <robcrowther@hotmail.com>
2022-09-10 12:54:34 +0100

    complete password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  password = '';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '!@£$%^&*()';
+    let validChars = '';
+    let generatedPassword = '';
+
+    if (this.includeLetters) validChars += letters;
+    if (this.includeNumbers) validChars += numbers;
+    if (this.includeSymbols) validChars += symbols;
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeIncludeLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeIncludeNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeIncludeSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
 }
